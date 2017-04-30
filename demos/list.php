e162f7b28625a1192aaa62dbebfd9dbe7424c86a
Robert Cox <robert@studiorclv.com>
2017-04-29 22:53:11 -0700

    Updated list command

---
+++ demos/list.php
@@ -0,0 +1,10 @@
+<?php
+
+echoLine("============================================================
+| Command             | Description                        |
+============================================================
+| php runner non-auth | sends a non authenticated request  |
+| php runner yes-auth | sends an authenticated request     |
+| php runner show-key | shows the api key currently in use |
+| php runner list     | lists the avaialbe commands        |
+============================================================");
