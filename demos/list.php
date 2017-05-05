634bd6932013e2db54af39795d4ee3d4c823d223
Robert Cox <robert@studiorclv.com>
2017-05-04 18:12:38 -0700

    Updated Examples

--- demos/list.php
+++ demos/list.php
@@ -1,10 +1,14 @@
 <?php
 
-echoLine("============================================================
-| Command             | Description                        |
-============================================================
-| php runner non-auth | sends a non authenticated request  |
-| php runner yes-auth | sends an authenticated request     |
-| php runner show-key | shows the api key currently in use |
-| php runner list     | lists the avaialbe commands        |
-============================================================");
+echoLine("==================================================================
+| Command                | Description                           |
+==================================================================
+| php runner get         | sends an authenticated get request    |
+| php runner put         | sends an authenticated put request    |
+| php runner post        | sends an authenticated post request   |
+| php runner update      | sends an authenticated update request |
+| php runner delete      | sends an authenticated delete request |
+| php runner noauth      | sends a non authenticated request     |
+| php runner show-key    | shows the api key currently in use    |
+| php runner list        | lists the avaialbe commands           |
+==================================================================");
